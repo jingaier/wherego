7d898b6c3625c48b269d35032bec47b858afc672
guoyongsheng <18638259796@139.com>
2020-02-12 23:02:21 +0800

    首页轮播图组件

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'// 全局使用 引用方法
 import fastClick from 'fastclick' /* 为了解决手机上 点击事件延迟300毫秒问题 */
 import 'styles/reset.css'// 移动端 样式重置
 import 'styles/border.css'// 为了解决 多倍屏 1像素问题
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css' /* //引入样式 */
 Vue.config.productionTip = false
 fastClick.attach(document.body)/* 使用 fastClick */
-
+Vue.use(VueAwesomeSwiper/*, { default global options } */)/* // 全局注册 可以全局使用 */
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="list of swiperLists" :key="list.id">
+        <img class="swiper-img" :src="list.imgUrl" />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+      <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div> -->
+    </swiper>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'HomeSwiper',
+    data () {
+      return {
+        swiperOption: {
+          // some swiper options/callbacks
+          // 所有的参数同 swiper 官方 api 参数
+          pagination: '.swiper-pagination'
+        },
+        swiperLists:[
+          {
+            id:'001',
+            imgUrl:'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/be6871c9b0b0ffab3ef2a86fa9688677.jpg'
+          },
+          {
+            id:'002',
+            imgUrl:'https://img1.qunarzz.com/sight/p0/1709/e9/e9b7ae23f567806ea3.img.jpg_600x330_339bdee5.jpg'
+          },
+          {
+            id:'003',
+            imgUrl:'https://img1.qunarzz.com/sight/p0/1712/d4/d4631cd43dff145fa3.img.png_600x330_96fe5fce.png'
+          }
+        ]
+      }
+    }
+  }
+</script>
+<style lang="stylus" >
+.wrapper >>> .swiper-pagination-bullet-active
+  background :red
+.wrapper
+  /* 这种方法 可以做到 宽高比例为31.25  */
+  width :100%
+  overflow :hidden
+  height:0
+  padding-bottom:31.25%
+  background :#ccc
+  .swiper-img
+    width:100%
+</style>
